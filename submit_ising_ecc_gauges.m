cc6406ebd98ebda60a6224579bb7379cba1df396
Anurag Mishra <anuragmi@usc.edu>
2017-06-14 14:48:55 -0700

    Add function to collect ECC problem with gauges
    Also fixed bug in the simple Ising data collection procedure.

--- submit_ising_ecc_gauges.m
+++ submit_ising_ecc_gauges.m
@@ -1,7 +1,8 @@
-function result = submit_ising_ecc(h_logical,J_logical,param,translateFnHandle,penalty)
-%SUBMIT_ISING_ECC submits an logical Ising instance to DWave and returns the raw result.
+function [result,gauges] = submit_ising_ecc_gauges(h_logical,J_logical,param,translateFnHandle,penalty)
+%SUBMIT_ISING_ECC_GAUGES submits an logical Ising instance to DWave and returns the result
+%                        in physical qubits..
 %USAGE:
-%   result = submit_ising_ecc(h_logical,J_logical,param,translateFnHandle,penalty)
+%   result = submit_ising_ecc_gauges(h_logical,J_logical,param,translateFnHandle,penalty)
 %
 %INPUT:
 %   h_logical  : The logical local fields.
@@ -9,9 +10,11 @@ function result = submit_ising_ecc(h_logical,J_logical,param,translateFnHandle,p
 %       Couplings will be scaled down to [-1,1] range if necessary. No scale up would be done.
 %   param      : parameters to be send to D-Wave
 %   tranlateFnHandle : A handle to function that can convert logical to physical Ham.
-%   penalty       : The required penalty couplings.
+%   penalty    : The required penalty couplings.
 %OUTPUT:
 %   result     : The raw result from D-Wave
+%   gauges     : The gauges used in problem submission. The raw results must be transformed
+%                to get answers in original gauge.
 
 %These parameters never change.
 param.auto_scale  = false;
@@ -26,6 +29,6 @@ h_logical = h_logical/scaleFactor; J_logical = J_logical/scaleFactor;
 [h_physical,J_physical] = translateFnHandle(h_logical,J_logical,penalty);
 
 %And submit the physical problem and return the result.
-result = dwGraph.submit_ising(h_physical,J_physical,param);
+[result,gauges] = dwGraph.submit_ising_gauges(h_physical,J_physical,param);
 
 end
